4c152f098ff75e616cce98df0eaa0b0aab4896c9
Elvis Cruz <teseosoftwarecompany@gmail.com>
2022-09-03 14:31:06 -0500

    Update settings.py

--- ShopCenter/ShopCenter/settings.py
+++ ShopCenter/ShopCenter/settings.py
@@ -145,7 +145,7 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="teseosoftwarecompany@gmail.com"
-EMAIL_HOST_PASSWORD="qcfgbztrorpeuhwo"
+EMAIL_HOST_PASSWORD=""
 
 
 CRISPY_TEMPLATE_PACK="bootstrap4"
